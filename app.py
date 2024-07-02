d5ede8dc10f6857891b5fa70282d9ea9c2aac053
Phoenixgreen763 <gpheen@yahoo.co.uk>
2024-07-02 18:19:01 +0100

    add favicon to base.html

--- app.py
+++ app.py
@@ -18,9 +18,6 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-
-
-
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
